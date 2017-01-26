fce0cf1f5abfd6e3990226209a2900607ef69b13
jers007 <esteban_ramirez@me.com>
2017-01-26 16:54:35 -0600

    Upload the first trial

---
+++ TestMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestMVC.Startup))]
+namespace TestMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
